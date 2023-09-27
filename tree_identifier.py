872d6763c24eaa5636efd4cf9601a3500a92e036
Dakota Marosi <124627789+dakotasteel@users.noreply.github.com>
2023-09-26 20:39:15 -0400

    Update tree_identifier.py

--- tree_identifier.py
+++ tree_identifier.py
@@ -5,29 +5,30 @@ CS 1210
 Program identifies a tree given user input option and using 
 conditionals
 """
+if __name__ == '__main__':
 
-leaves_or_needles = input("Does it have leaves (l) or needles (n)? ")
-leaves_or_needles = leaves_or_needles.lower()
+    leaves_or_needles = input("Does it have leaves (l) or needles (n)? ")
+    leaves_or_needles = leaves_or_needles.lower()
 
-if leaves_or_needles == "l":
-    simple_or_compound = input("Are the leaves simple (s) or compound (c)? ")
-    simple_or_compound = simple_or_compound.lower()
-    if simple_or_compound == "s":
-        print("Maple")
-    elif simple_or_compound == "c":
-        print("Ash")
+    if leaves_or_needles == "l":
+        simple_or_compound = input("Are the leaves simple (s) or compound (c)? ")
+        simple_or_compound = simple_or_compound.lower()
+        if simple_or_compound == "s":
+            print("Maple")
+        elif simple_or_compound == "c":
+            print("Ash")
+        else:
+            print("Invalid choice!")
+    elif leaves_or_needles == "n":
+        individ_or_clust = input("Are needles individual or clustered (i or c)? ")
+        individ_or_clust = individ_or_clust.lower()
+        if individ_or_clust == "i":
+            print("Spruce")
+        elif individ_or_clust == "c":
+            print("Pine")
+        else:
+            print("Invalid choice!")
     else:
         print("Invalid choice!")
-elif leaves_or_needles == "n":
-    individ_or_clust = input("Are needles individual or clustered (i or c)? ")
-    individ_or_clust = individ_or_clust.lower()
-    if individ_or_clust == "i":
-        print("Spruce")
-    elif individ_or_clust == "c":
-        print("Pine")
-    else:
-        print("Invalid choice!")
-else:
-    print("Invalid choice!")
 
 
