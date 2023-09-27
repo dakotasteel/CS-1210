0c299e91c70a3d1b8f55ae4fb511e4dda93db4ab
Dakota Marosi <124627789+dakotasteel@users.noreply.github.com>
2023-09-26 20:12:41 -0400

    Add files via upload

---
+++ quadratic_solver.py
@@ -0,0 +1,28 @@
+"""
+Quadratic Solver
+Dakota Marosi
+CS 1210
+Program finds roots of a quadratic polynomial, if any
+"""
+
+import math
+
+a = float(input("Enter value for a: "))
+b = float(input("Enter value for b: "))
+c = float(input("Enter value for c: "))
+
+def calc_discriminant(x, y, z):
+    discriminant = (y**2 - (4 * x * z))
+    return discriminant
+
+result = calc_discriminant(a, b, c)
+if result > 0:
+    root_one = (-b + math.sqrt(result))/(2 * a)
+    root_two = (-b - math.sqrt(result))/(2 * a)
+    print(f"The two roots of this equation are {root_one:.3f}  and  {root_two:.3f}")
+elif result == 0:
+    single_root = (-b / (2 * a))
+    print(f"There is one real root to this equation {single_root:.3f}")
+else:
+    print("There are no real roots to this equation.")
+        
