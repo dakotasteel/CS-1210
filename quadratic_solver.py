0a8261f46161f1e49ccec64f8b18b6d5ff59da10
Dakota Marosi <124627789+dakotasteel@users.noreply.github.com>
2023-09-26 20:26:27 -0400

    Update quadratic_solver.py

--- quadratic_solver.py
+++ quadratic_solver.py
@@ -11,16 +11,19 @@ a = float(input("Enter value for a: "))
 b = float(input("Enter value for b: "))
 c = float(input("Enter value for c: "))
 
+
 def calc_discriminant(x, y, z):
-    discriminant = (y**2 - (4 * x * z))
+    discriminant = (y ** 2 - (4 * x * z))
     return discriminant
 
-result = calc_discriminant(a, b, c)
-if result > 0:
-    root_one = (-b + math.sqrt(result))/(2 * a)
-    root_two = (-b - math.sqrt(result))/(2 * a)
-    print(f"The two roots of this equation are {root_one:.3f}  and  {root_two:.3f}")
-elif result == 0:
+
+d = calc_discriminant(a, b, c)
+if d > 0:
+    root_one = (-b + math.sqrt(d))/(2 * a)
+    root_two = (-b - math.sqrt(d))/(2 * a)
+    print(f"The two roots of this equation are \
+          {root_one:.3f}  and  {root_two:.3f}")
+elif d == 0:
     single_root = (-b / (2 * a))
     print(f"There is one real root to this equation {single_root:.3f}")
 else:
