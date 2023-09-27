6d27f5792c2fdc8f80e347b3b8a9b8a909f6b50b
Dakota Marosi <124627789+dakotasteel@users.noreply.github.com>
2023-09-27 15:28:08 -0400

    Update quadratic_solver.py

--- quadratic_solver.py
+++ quadratic_solver.py
@@ -7,26 +7,23 @@ Program finds roots of a quadratic polynomial, if any
 
 import math
 
+def calc_discriminant(a, b, c):
+    discriminant = b ** 2 - 4 * a * c
+    return discriminant
+
 if __name__ == '__main__':
 
     a = float(input("Enter value for a: "))
     b = float(input("Enter value for b: "))
     c = float(input("Enter value for c: "))
 
-
-    def calc_discriminant(a, b, c):
-        calc_discriminant = (b ** 2 - (4 * a * c))
-        return calc_discriminant
-
-
     d = calc_discriminant(a, b, c)
     if d > 0:
-        root_one = (-b + math.sqrt(d))/(2 * a)
-        root_two = (-b - math.sqrt(d))/(2 * a)
-        print(f"This equations two roots are {root_one:.3f} and {root_two:.3f}")
+        root_one = (- b + math.sqrt(d)) / (2 * a)
+        root_two = (- b - math.sqrt(d)) / (2 * a)
+        print(f"This equation's roots are {root_one:.3f} and {root_two:.3f}")
     elif d == 0:
         single_root = (-b / (2 * a))
         print(f"There is one real root to this equation {single_root:.3f}")
     else:
         print("There are no real roots to this equation.")
-            
