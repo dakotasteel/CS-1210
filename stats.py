2253e8ce86652ea25ddcc3395c55e93e48214750
Dakota Marosi <124627789+dakotasteel@users.noreply.github.com>
2023-10-12 12:18:03 -0400

    Update stats.py
    fixed bug

--- stats.py
+++ stats.py
@@ -2,8 +2,7 @@
 Euclidean Division
 Dakota Marosi
 CS 1210
-Program calculates average and standard deviation 
-of user inputted numbers
+Program does Euclidean divison by repeated subtraction, a la the old days
 """
 
 def mean(lst):
@@ -15,22 +14,20 @@ def mean(lst):
 
 def std_dev(lst):
     sum = []
-    mean = 0
-    
-    for _ in range(5):
-        mean +=  lst[_]
-    mean = mean / 5
+    average = 0
 
     for _ in range(5):
-        new_square = (lst[_] - mean) ** 2
+        average +=  lst[_]
+    average = average / 5
+
+    for _ in range(5):
+        new_square = (lst[_] - average) ** 2
         sum.append(new_square)
     
     sum_tot = 0
     for _ in range(5):
         sum_tot += sum[_]
-        print(sum_tot)
     sum_tot = sum_tot/5
-    print(sum_tot)
     return sum_tot
 
 if __name__ == "__main__":
