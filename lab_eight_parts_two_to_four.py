20f2b595741b77e5de5eb35dbf84b8da282c56f2
Dakota Marosi <124627789+dakotasteel@users.noreply.github.com>
2023-10-25 16:29:18 -0400

    Create lab_eight_parts_two_to_four.py

---
+++ lab_eight_parts_two_to_four.py
@@ -0,0 +1,12 @@
+"""
+CS 1210
+Lab #8
+Dakota Marosi
+October 25th, 2023
+"""
+if __name__ == "__main__":
+    counter = 0
+    with open('part_two.py', 'r') as f:
+        for row in f:
+            counter += 1
+            print(f'{counter}: {row}')
