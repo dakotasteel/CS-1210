ea41e5b27245cf693a608de0a96e5849abb0eeb7
Dakota Marosi <124627789+dakotasteel@users.noreply.github.com>
2023-10-31 23:43:09 -0400

    Update co2_ppm.py

--- co2_ppm.py
+++ co2_ppm.py
@@ -6,16 +6,13 @@ CS 1210
 import csv
 
 def mean(lst):
-    mean = 0.0
+    mean = 0
     counter = 0
     for i in lst:
         counter += 1
-        mean += lst[i]
-        print(counter)
-        print(mean)
+        mean +=  lst[i]
     mean = mean / counter
-    print(mean)
-    return(mean)
+    return mean
     
     # Calculate and return the mean of the argument supplied for lst.
 
@@ -26,14 +23,16 @@ def std_dev(lst):
     # of the argument supplied for lst.
 
 if __name__ == "__main__":
-    lst = []
+    lst1 = []
+    lst2 = []
     with open('co2_ppm.csv', newline = '') as f:
         reader = csv.reader(f)
         for row in reader:
-            lst.append(row)
-        print(lst)
+            lst1.append(row[1:])
+            lst2.append(row[0:1])
+
+    print(lst1)
 
     # Read data from file into a list of floats
     # Function call(s) to `mean()` and `std_dev()` as needed.
     # Write results to file.
-
