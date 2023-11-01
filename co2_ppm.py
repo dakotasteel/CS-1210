8cca1485cd66307483790b3cc907befaba581c01
Dakota Marosi <124627789+dakotasteel@users.noreply.github.com>
2023-11-01 00:20:09 -0400

    Update co2_ppm.py

--- co2_ppm.py
+++ co2_ppm.py
@@ -4,13 +4,14 @@ Dakota Marosi
 CS 1210
 """
 import csv
+import math
 
 def mean(lst):
     mean = 0
     counter = 0
-    for i in lst:
+    for _ in range(185):
         counter += 1
-        mean +=  lst[i]
+        mean +=  int(lst[_:])
     mean = mean / counter
     return mean
     
@@ -18,9 +19,23 @@ def mean(lst):
 
 
 def std_dev(lst):
-    print()
-    # Calculate and return the standard deviation 
-    # of the argument supplied for lst.
+    sum = []
+    average = 0
+
+    for _ in range(5):
+        average +=  lst[_]
+    average = average / 5
+
+    for _ in range(5):
+        new_square = (lst[_] - average) ** 2
+        sum.append(new_square)
+    
+    sum_tot = 0
+    for _ in range(5):
+        sum_tot += sum[_]
+    sum_tot = sum_tot/5
+    sum_tot = math.sqrt(sum_tot)
+    return sum_tot
 
 if __name__ == "__main__":
     lst1 = []
@@ -31,7 +46,7 @@ if __name__ == "__main__":
             lst1.append(row[1:])
             lst2.append(row[0:1])
 
-    print(lst1)
+    mean(lst1)
 
     # Read data from file into a list of floats
     # Function call(s) to `mean()` and `std_dev()` as needed.
