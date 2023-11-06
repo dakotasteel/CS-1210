b7029db1203785d0d69853f2268a71393b0f8a82
Dakota Marosi <124627789+dakotasteel@users.noreply.github.com>
2023-11-06 16:14:56 -0500

    Update lab_eight_part_two.py
    fixed error in code

--- lab_eight_part_two.py
+++ lab_eight_part_two.py
@@ -4,9 +4,13 @@ Lab #8
 Dakota Marosi
 October 25th, 2023
 """
+import csv
+
 if __name__ == "__main__":
     counter = 0
     with open('part_two.py', 'r') as f:
         for line in f:
             counter += 1
-            print(f'{counter}: {line}')
+            string = f'{counter:>4}: {line}'
+            stripped = string.strip('\n')
+            print(stripped)
