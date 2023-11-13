0add3fa6c878005ffd7a1ce1d8582a99b4adeae2
Dakota Marosi <124627789+dakotasteel@users.noreply.github.com>
2023-11-13 16:25:42 -0500

    Create nov13_class.py

---
+++ nov13_class.py
@@ -0,0 +1,13 @@
+"""
+CS 1210
+In Class Exercise
+Dakota Marosi
+November 13th, 2023
+Dictionary Practice
+"""
+
+student_profiles = {'Porcupine, Egbert' : {'NETID' : 'eporcupi',
+                                           'MAJOR' : 'CS',
+                                           'COURSES' : 'CS1210, CS1080, MATH2055, ANTH1100'}}
+
+
