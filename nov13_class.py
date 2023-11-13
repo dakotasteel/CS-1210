c5e8420cb4dde6ff05d1a9ee458609d009fbeff1
Dakota Marosi <124627789+dakotasteel@users.noreply.github.com>
2023-11-13 16:45:29 -0500

    Update nov13_class.py

--- nov13_class.py
+++ nov13_class.py
@@ -6,6 +6,7 @@ November 13th, 2023
 Dictionary Practice
 """
 
+# Practice with nested dictionaries
 student_profiles = {'eporcupi' : 
                     {'NAME' : {'first' : 'Egbert',
                               'last' : 'Porcupine'},
@@ -33,3 +34,16 @@ student_profiles = {'eporcupi' :
                     'MAJOR' : 'ENSC',
                     'COURSES' : ('ENSC2300', 'GEOG1170',
                                 'FS2020', 'STAT2870')}}
+
+''' 
+Bercause dictionaries are mutable, like a list, they will still be modified
+in the outside scope, even if you return something like a None value.
+The below is an example of this! :)
+'''
+
+d = {'foo': 'bar'}
+def modify_dictionary(d_):
+    d_['new_item'] = 'new_value'
+    return None
+modify_dictionary(d)
+print(d)
