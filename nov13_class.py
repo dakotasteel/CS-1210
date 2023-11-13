765cf78a3fb0dfb98b8901819dfd107f9e2e6ab8
Dakota Marosi <124627789+dakotasteel@users.noreply.github.com>
2023-11-13 16:39:06 -0500

    Update nov13_class.py

--- nov13_class.py
+++ nov13_class.py
@@ -6,8 +6,30 @@ November 13th, 2023
 Dictionary Practice
 """
 
-student_profiles = {'Porcupine, Egbert' : {'NETID' : 'eporcupi',
-                                           'MAJOR' : 'CS',
-                                           'COURSES' : 'CS1210, CS1080, MATH2055, ANTH1100'}}
+student_profiles = {'eporcupi' : 
+                    {'NAME' : {'first' : 'Egbert',
+                              'last' : 'Porcupine'},
+                    'MAJOR' : 'CS',
+                    'COURSES' : ('CS1210', 'CS1080', 
+                                 'MATH2055', 'ANTH1100')},
 
-
+                    'epickle' : 
+                    {'NAME' : {'first' : 'Edwina',
+                              'last' : 'Pickle'},
+                    'MAJOR' : 'BIOL',
+                    'COURSES' : ('BIOL1450', 'BIOL1070', 
+                                 'MATH2248', 'CHEM1150')},
+                    
+                    'wquux' : 
+                    {'NAME' : {'first' : 'Winston',
+                               'last' : 'Quux'},
+                    'MAJOR' : 'ARTS',
+                    'COURSES' : ('ARTS2100', 'ARTS2750', 
+                                 'CS1210', 'ARTH1990')},
+                    
+                    'mgarply' : 
+                    {'NAME' : {'first' : 'Mephista',
+                               'last' : 'Garply'},
+                    'MAJOR' : 'ENSC',
+                    'COURSES' : ('ENSC2300', 'GEOG1170',
+                                'FS2020', 'STAT2870')}}
