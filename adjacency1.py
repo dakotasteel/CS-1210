c967833f1b24edaa2752b1ed4398daa9d5f7f798
Dakota Marosi <124627789+dakotasteel@users.noreply.github.com>
2023-11-15 16:35:40 -0500

    Create adjacency1.py

---
+++ adjacency1.py
@@ -0,0 +1,16 @@
+"""
+CS 1210
+Lab #10
+Dakota Marosi
+November 15th, 2023
+"""
+
+if __name__ == "__main__":
+    FRIENDS = {"Albert" : ["Victor", "Egbert"],
+               "Victor" : ["Albert", "Farrah", "Qani", "Ichabod"],
+               "Egbert" : ["Albert", "Farrah", "Qani"],
+               "Ichabod": ["Qani", "Farrah", "Victor"],
+               "Qani" : ["Ichabod", "Egbert", "Victor"],
+               "Farrah" : ["Ichabod", "Egbert", "Victor"],
+               "Kamala" : []}
+  
