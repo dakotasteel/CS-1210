88d8ed777c32a2c839209bfe0ea2e47d2cf3c108
Dakota Marosi <124627789+dakotasteel@users.noreply.github.com>
2023-11-14 23:41:34 -0500

    Create read_file.py

---
+++ read_file.py
@@ -0,0 +1,25 @@
+"""
+Homework 10
+November 14th
+Dakota Marosi
+CS 1210
+"""
+
+if __name__ == '__main__':
+    while True:
+        try:
+            get_name = input("Enter name of file you want to open:")
+            file = open(get_name, "r")
+            
+            counter = 0
+            for line in file:
+                counter += 1
+                print(line)
+            
+            if counter > 0:
+                break
+
+        except FileNotFoundError:
+            if get_name == "q":
+                break
+            print(f'"{get_name}" cannot be found in this directory. Please enter a new file name.')
