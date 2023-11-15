01381eb3849ba9b65080b56497a11c0de56a2f1b
Dakota Marosi <124627789+dakotasteel@users.noreply.github.com>
2023-11-14 20:57:41 -0500

    Create input_validation.py

---
+++ input_validation.py
@@ -0,0 +1,56 @@
+"""
+Dakota Marosi
+
+There's a problem with input validation. We want the user to be able
+to enter floating-point numbers for mass 1, mass 2, and r.
+
+Fix this program so that it responds appropriately for each individual input
+provided by the user. You will need to convert inputs to floats, and respond
+with an appropriate message if the user enters a value which cannot be
+converted to a float.
+
+Remember that if the user enters an integer it can be converted to a
+float with float(). E.g., float(1) => 1.0.
+"""
+
+G = 6.67430 * 10 ** -11
+
+
+def calc_force(m1, m2, r):
+    """Given masses in kg, and distance in meters, returns gravitational
+    force in Newtons. """
+    return G * (m1 * m2) / r ** 2
+
+
+if __name__ == '__main__':
+    while True:
+        try:
+            m_01 = input("Enter the mass of body 01 in kg: ")
+            m_01 = int(m_01)
+            if m_01 >= 0:
+                break
+        except ValueError:
+            print(f'"{m_01}" cannot be converted to an int!')
+
+    while True:
+        try:
+            m_02 = input("Enter the mass of body 02 in kg: ")
+            m_02 = int(m_02)
+            if m_02 >= 0:
+                break
+        except ValueError:
+            print(f'"{m_02}" cannot be converted to an int!')
+
+    while True:
+        try:
+            d = input("Enter the distance between the centers of the bodies in m: ")
+            d = int(d)
+            if d >= 0:
+                break
+        except ValueError:
+            print(f'"{d}" cannot be converted to an int!')
+
+
+    f = calc_force(m_01, m_02, d)
+
+    print(f"The gravitational force is {f:,.4f} Newtons")
