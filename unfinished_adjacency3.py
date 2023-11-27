631e1735fb93cfc41193b3360743c1338c219297
Dakota Marosi <124627789+dakotasteel@users.noreply.github.com>
2023-11-27 15:42:28 -0500

    Create unfinished_adjacency3.py

---
+++ unfinished_adjacency3.py
@@ -0,0 +1,14 @@
+"""
+CS 1210
+Lab #10
+Dakota Marosi
+November 15th, 2023
+"""
+
+if __name__ == "__main__":
+    PEOPLE = ['Albert', 'Bethany', 'Chandra', 'Dorothy', 'Egbert', 
+          'Farrah', 'Gikuyu', 'Horace', 'Ichabod', 'Jane', 
+          'Kamala', 'Langston', 'Minerva', 'Nestor', 'Olajuwan', 
+          'Piotr', 'Qani', 'Rakesh', 'Soren', 'Tilde', 'Ursula', 
+          'Victor', 'Winston', 'Xavier', 'Yolanda', 'Zoe']
+    
