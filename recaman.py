2d0bb0efa3a0da3537d5bafc28741a3419970e17
Dakota Marosi <124627789+dakotasteel@users.noreply.github.com>
2023-12-05 17:02:02 -0500

    Update recaman.py
    Edited program so that, while still producing Recaman's sequence, the function specifically returns the next number

--- recaman.py
+++ recaman.py
@@ -4,14 +4,14 @@ CS1210
 Homework 12 (Extra Credit)
 """
 
+
 def next_recaman(lst):
     if len(lst) == 0:
         lst.append(0)
-        return lst
+        return lst[-1]
     elif (lst[-1] - len(lst)) > 0 and (lst[-1] - len(lst)) not in lst:
         lst.append(lst[-1] - len(lst))
-        return lst
+        return lst[-1]
     else:
         lst.append(lst[-1] + len(lst))
-        return lst
-
+        return lst[-1]
